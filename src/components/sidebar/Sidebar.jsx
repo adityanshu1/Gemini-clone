576ce67a27b7697116682f5400193fce582559b5
adityanshu1 <adityanshupadhy3@gmail.com>
2025-08-09 22:04:12 +0530

    rrrr

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,57 @@
+import React, { useContext, useState } from 'react';
+import './Sidebar.css'; 
+import {assets} from '../../assets/assets'
+import { Context } from '../../context/context';
+const Sidebar = () => {
+const[extend,setExtended] = useState(false)
+const {onSent,prevPrompts,setRecentPrompts,newChat} = useContext(Context)
+
+const loadPrompt= async(prompt) =>{
+  setRecentPrompts(prompt)
+ await onSent(prompt)
+}
+
+  return (
+    
+    <div className='sidebar'>
+        <div className="top">
+            
+          <img onClick={()=>setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" />
+          <div onClick={()=>newChat()} className="new-chat">
+            <img  className='plus' src={assets.plus_icon} alt="" />
+            {extend?<p>New Chat</p> : null}
+          </div>
+          {extend? 
+          <div className="recent">
+            <p className='recent-title'>Recent</p>
+            {prevPrompts.map((item,index)=>{
+               return(
+                  <div onClick={()=>loadPrompt(item)} className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>{item.slice(0,18)}...</p>
+            </div>
+               )
+            })}
+            
+          </div>:null}
+          
+        </div>
+        <div className="bottom">
+          <div className="bottom_item recent-entry">
+            <img src={assets.question_icon} alt="" />
+            {extend?<p>Help</p>:null}
+          </div>
+          <div className="bottom_item recent-entry">
+            <img src={assets.history_icon} alt="" />
+            {extend?<p>Activity</p>:null}
+          </div>
+          <div className="bottom_item recent-entry">
+            <img src={assets.setting_icon} alt="" />
+            {extend?<p>Settings</p>:null}
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
